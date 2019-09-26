cfb91ded955d0e8d4de9ac37cb536637259ae8f9
Alok Tripathy <kullalok@gmail.com>
2019-09-25 21:51:49 -0400

    Added experiment scripts

--- hash-graph-dehornetify/experiments/key_scalability.sh
+++ hash-graph-dehornetify/experiments/key_scalability.sh
@@ -1,18 +1,30 @@
-keycounts=($(seq 25 1 35))
+keycounts=($(seq 25 1 34))
 
 execpath="../build"
 resultsfile="./results/key_scalability.txt"
 
-gpucount=2
+gpucount=16
 bincount=16000
 tablesize=($(echo "2^31 - 1" | bc))
 
+echo "build tests"
+echo "build tests" >> $resultsfile
 for i in "${keycounts[@]}"
     do
         echo $i
         echo $i >> $resultsfile
-        ./$execpath/multi-hash $i $tablesize $bincount $gpucount $bincount nocheck $i | grep "time" >> $resultsfile
+        ./$execpath/multi-hash $i $tablesize $bincount $gpucount $bincount nocheck $i build | grep "time" >> $resultsfile
     done
 echo "" >> $resultsfile
-#for i in 
-#make multi-hash && ./multi-hash 16777216 16777216 16000 2 16000 check 16777216
+
+echo "intersect tests"
+echo "intersect tests" >> $resultsfile
+
+tablesize=($(echo "2^31 - 1" | bc ))
+for i in "${keycounts[@]}"
+    do
+        echo $i
+        echo $i >> $resultsfile
+        ./$execpath/multi-hash $(echo "2^32" | bc) $tablesize $bincount $gpucount $bincount nocheck $i intersect | grep "time" >> $resultsfile
+    done
+echo "" >> $resultsfile
