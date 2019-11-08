288c7cf68b24113f77c2eabd28721907d5eabffc
Alok Tripathy <aloktripathynj@gmail.com>
2019-11-08 15:14:08 -0500

    Removed file writing from experiment scripts

--- hash-graph-dehornetify/experiments/duplicate_keys/duplicate_keys.sh
+++ hash-graph-dehornetify/experiments/duplicate_keys/duplicate_keys.sh
@@ -1,5 +1,6 @@
 keycount=26
-gpucount=4
+# gpucount=4
+gpucount=6
 tablesizes=($(seq 21 1 31))
 
 execpath="../build"
@@ -8,43 +9,49 @@ resultsfile=$1
 bincount=16000
 
 # sed -i 's/^\/\/.*#define INDEX_TRACK/#define INDEX_TRACK/' ../../include/MultiHashGraph.cuh
-make -C $execpath multi-hash
+# make -C $execpath multi-hash
 
 rm $resultsfile
-echo "keycount,tablesize,gpucount,time" >> $resultsfile
+# echo "keycount,tablesize,gpucount,time" >> $resultsfile
+echo "duplicate_keys"
+echo "keycount,tablesize,gpucount,time"
 echo "build tests"
-echo "build tests" >> $resultsfile
+# echo "build tests" >> $resultsfile
 
 for i in "${tablesizes[@]}"
     do
         let kc=$((echo 2^$keycount) | bc)
-        let gc=$((echo 2^$gpucount) | bc)
+        # let gc=$((echo 2^$gpucount) | bc)
+        let gc=$gpucount
         let ts=$((echo 2^$i) | bc)
 
-        echo "tableSize: ${ts}"
+        # echo "tableSize: ${ts}"
         ans=$(./$execpath/multi-hash $kc $ts $bincount $gc $bincount nocheck $kc build | grep "time")
 
         tokens=( $ans )
         time=${tokens[3]}
 
-        echo "${kc},${ts},${gc},${time}" >> $resultsfile
+        # echo "${kc},${ts},${gc},${time}" >> $resultsfile
+        echo "${kc},${ts},${gc},${time}"
     done
 
 echo "intersect tests"
-echo "intersect tests" >> $resultsfile
+# echo "intersect tests" >> $resultsfile
 
 keycount=$((echo $keycount - 1) | bc)
 for i in "${tablesizes[@]}"
     do
         let kc=$((echo 2^$keycount) | bc)
-        let gc=$((echo 2^$gpucount) | bc)
+        # let gc=$((echo 2^$gpucount) | bc)
+        let gc=$gpucount
         let ts=$((echo 2^$i) | bc)
 
-        echo "tableSize: ${ts}"
+        # echo "tableSize: ${ts}"
         ans=$(./$execpath/multi-hash $kc $ts $bincount $gc $bincount nocheck $kc intersect | grep "time")
 
         tokens=( $ans )
         time=${tokens[3]}
 
-        echo "${kc},${ts},${gc},${time}" >> $resultsfile
+        # echo "${kc},${ts},${gc},${time}" >> $resultsfile
+        echo "${kc},${ts},${gc},${time}"
     done
--- hash-graph-dehornetify/experiments/strong_scaling/strong_scaling.sh
+++ hash-graph-dehornetify/experiments/strong_scaling/strong_scaling.sh
@@ -1,5 +1,6 @@
 keycounts=($(seq 24 1 33))
-gpucounts=($(seq 0 1 4))
+# gpucounts=($(seq 0 1 4))
+gpucounts=(1 2 4 6)
 
 execpath="../build"
 resultsfile=$1
@@ -7,48 +8,54 @@ resultsfile=$1
 bincount=16000
 
 # sed -i 's/^\/\/.*#define INDEX_TRACK/#define INDEX_TRACK/' ../../include/MultiHashGraph.cuh
-make -C $execpath multi-hash
+# make -C $execpath multi-hash
 
 rm $resultsfile
-echo "keycount,gpucount,time" >> $resultsfile
+# echo "keycount,gpucount,time" >> $resultsfile
+echo "strong_scaling"
+echo "keycount,gpucount,time"
 echo "build tests"
-echo "build tests" >> $resultsfile
+# echo "build tests" >> $resultsfile
 
 for i in "${keycounts[@]}"
     do
         let kc=$((echo 2^$i) | bc)
-        echo "keycount: ${kc}"
+        # echo "keycount: ${kc}"
         for j in "${gpucounts[@]}"
             do
-                let gc=$((echo 2^$j) | bc)
-                echo "gpucount: ${gc}"
+                # let gc=$((echo 2^$j) | bc)
+                let gc=$j
+                # echo "gpucount: ${gc}"
 
                 ans=$(./$execpath/multi-hash $kc $kc $bincount $gc $bincount nocheck $kc build | grep "time")
                 tokens=( $ans )
                 time=${tokens[3]}
 
-                echo "${kc},${gc},${time}" >> $resultsfile
+                # echo "${kc},${gc},${time}" >> $resultsfile
+                echo "${kc},${gc},${time}"
             done
     done
 
 echo "intersect tests"
-echo "intersect tests" >> $resultsfile
+# echo "intersect tests" >> $resultsfile
 
 for i in "${keycounts[@]}"
     do
         let kc=$((echo 2^$i) | bc)
         kc=$((kc / 2))
-        echo "keycount: ${kc}"
+        # echo "keycount: ${kc}"
         for j in "${gpucounts[@]}"
             do
-                let gc=$((echo 2^$j) | bc)
-                echo "gpucount: ${gc}"
+                # let gc=$((echo 2^$j) | bc)
+                let gc=$j
+                # echo "gpucount: ${gc}"
 
                 ans=$(./$execpath/multi-hash $kc $kc $bincount $gc $bincount nocheck $kc intersect | grep "time")
                 tokens=( $ans )
                 time=${tokens[3]}
 
-                echo "${kc},${gc},${time}" >> $resultsfile
+                # echo "${kc},${gc},${time}" >> $resultsfile
+                echo "${kc},${gc},${time}"
             done
     done
 
