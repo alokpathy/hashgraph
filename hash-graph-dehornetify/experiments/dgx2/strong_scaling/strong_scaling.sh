5edfd28debe65bd467980fd42bae136cebfd21ce
Alok Tripathy <kullalok@gmail.com>
2019-12-17 14:16:03 -0500

    Fixed memory heuristic in dgx2 scripts

--- hash-graph-dehornetify/experiments/dgx2/strong_scaling/strong_scaling.sh
+++ hash-graph-dehornetify/experiments/dgx2/strong_scaling/strong_scaling.sh
@@ -29,13 +29,13 @@ for i in "${keycounts[@]}"
                 # echo "gpucount: ${gc}"
 
                 # internal cuda malloc + keys + hashes + keyBinBuff
-                # let gigs=$((echo "((($kc * $1) + ($kc * 8) + (2 * $kc * $1) + (2 * $kc * 8)) + ($kc * 8) + ($kc * 8) + ($kc * $1)) / 2^30") | bc)
-                # let gpureq=$((echo "($gigs + 16) / 16") | bc)
+                let gigs=$((echo "((($kc * $1) + ($kc * 8) + (2 * $kc * $1) + (2 * $kc * 8)) + ($kc * 8) + ($kc * 8) + ($kc * $1)) / 2^30") | bc)
+                let gpureq=$((echo "($gigs + 32) / 32") | bc)
 
-                # if (( $gpureq > $gc )) ; then
-                #   echo "${kc},${gc},oom"
-                #   continue
-                # fi
+                if (( $gpureq > $gc )) ; then
+                  echo "${kc},${gc},oom"
+                  continue
+                fi
                 
                 ans=$(./$execpath/multi-hash $kc $kc $bincount $gc $bincount nocheck $kc build | grep "time")
                 tokens=( $ans )
@@ -60,13 +60,13 @@ for i in "${keycounts[@]}"
                 # echo "gpucount: ${gc}"
 
                 # internal cuda malloc + keys + hashes + keyBinBuff
-                # let gigs=$((echo "((($kc * $1) + ($kc * 8) + (2 * $kc * $1) + (2 * $kc * 8)) + ($kc * 8) + ($kc * 8) + ($kc * $1)) / 2^30") | bc)
-                # let gpureq=$((echo "($gigs * 2 + 16) / 16") | bc)
+                let gigs=$((echo "((($kc * $1) + ($kc * 8) + (2 * $kc * $1) + (2 * $kc * 8)) + ($kc * 8) + ($kc * 8) + ($kc * $1)) / 2^30") | bc)
+                let gpureq=$((echo "($gigs * 2 + 32) / 32") | bc)
 
-                # if (( $gpureq > $gc )) ; then
-                #   echo "${kc},${gc},oom"
-                #   continue
-                # fi
+                if (( $gpureq > $gc )) ; then
+                  echo "${kc},${gc},oom"
+                  continue
+                fi
 
                 ans=$(./$execpath/multi-hash $kc $kc $bincount $gc $bincount nocheck $kc intersect | grep "time")
                 tokens=( $ans )
--- hash-graph-dehornetify/experiments/dgx2/weak_scaling/weak_scaling.sh
+++ hash-graph-dehornetify/experiments/dgx2/weak_scaling/weak_scaling.sh
@@ -31,13 +31,13 @@ for i in "${keycounts[@]}"
                 let kc=$(($kcdev * $gc))
 
                 # internal cuda malloc + keys + hashes + keyBinBuff
-                # let gigs=$((echo "((($kc * $1) + ($kc * 8) + (2 * $kc * $1) + (2 * $kc * 8)) + ($kc * 8) + ($kc * 8) + ($kc * $1)) / 2^30") | bc)
-                # let gpureq=$((echo "($gigs + 16) / 16") | bc)
+                let gigs=$((echo "((($kc * $1) + ($kc * 8) + (2 * $kc * $1) + (2 * $kc * 8)) + ($kc * 8) + ($kc * 8) + ($kc * $1)) / 2^30") | bc)
+                let gpureq=$((echo "($gigs + 32) / 32") | bc)
 
-                # if (( $gpureq > $gc )) ; then
-                #   echo "${kc},${gc},oom"
-                #   continue
-                # fi
+                if (( $gpureq > $gc )) ; then
+                  echo "${kc},${gc},oom"
+                  continue
+                fi
 
                 ans=$(./$execpath/multi-hash $kc $kc $bincount $gc $bincount nocheck $kc build | grep "time")
                 tokens=( $ans )
@@ -65,13 +65,13 @@ for i in "${keycounts[@]}"
                 let kc=$(($kcdev * $gc))
 
                 # # internal cuda malloc + keys + hashes + keyBinBuff
-                # let gigs=$((echo "((($kc * $1) + ($kc * 8) + (2 * $kc * $1) + (2 * $kc * 8)) + ($kc * 8) + ($kc * 8) + ($kc * $1)) / 2^30") | bc)
-                # let gpureq=$((echo "($gigs * 2 + 16) / 16") | bc)
+                let gigs=$((echo "((($kc * $1) + ($kc * 8) + (2 * $kc * $1) + (2 * $kc * 8)) + ($kc * 8) + ($kc * 8) + ($kc * $1)) / 2^30") | bc)
+                let gpureq=$((echo "($gigs * 2 + 32) / 32") | bc)
 
-                # if (( $gpureq > $gc )) ; then
-                #   echo "${kc},${gc},oom"
-                #   continue
-                # fi
+                if (( $gpureq > $gc )) ; then
+                  echo "${kc},${gc},oom"
+                  continue
+                fi
 
                 ans=$(./$execpath/multi-hash $kc $kc $bincount $gc $bincount nocheck $kc intersect | grep "time")
                 tokens=( $ans )
