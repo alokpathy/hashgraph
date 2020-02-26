e98438125d8bd901f7674103702249cfef96d9d9
Alok Tripathy <kullalok@gmail.com>
2020-02-26 00:14:21 -0500

    New dgx2 plots

---
+++ hash-graph-dehornetify/experiments/dgx2_old/duplicate_keys/duplicate_keys.sh
@@ -0,0 +1,73 @@
+keycount=33
+gpucount=16
+tablesizes=($(seq 21 1 33))
+
+execpath="../../build"
+resultsfile=$1
+
+bincount=16000
+
+# sed -i 's/^\/\/.*#define INDEX_TRACK/#define INDEX_TRACK/' ../../include/MultiHashGraph.cuh
+# make -C $execpath multi-hash
+
+# rm $resultsfile
+# echo "keycount,tablesize,gpucount,time" >> $resultsfile
+echo "duplicate_keys"
+echo "keycount,tablesize,gpucount,time"
+echo "build tests"
+# echo "build tests" >> $resultsfile
+
+for i in "${tablesizes[@]}"
+    do
+        let kc=$((echo 2^$keycount) | bc)
+        # let gc=$((echo 2^$gpucount) | bc)
+        let gc=$gpucount
+        let ts=$((echo 2^$i) | bc)
+
+        # echo "tableSize: ${ts}"
+        # internal cuda malloc + keys + hashes + keyBinBuff
+        let gigs=$((echo "((($kc * $1) + ($kc * 8) + (2 * $kc * $1) + (2 * $ts * 8)) + ($kc * 8) + ($kc * 8) + ($kc * $1)) / 2^30") | bc)
+        let gpureq=$((echo "($gigs + 32) / 32") | bc)
+
+        if (( $gpureq > $gc )) ; then
+          echo "${kc},${gc},oom"
+          continue
+        fi
+
+        ans=$(./$execpath/multi-hash $kc $ts $bincount $gc $bincount nocheck $kc build | grep "time")
+
+        tokens=( $ans )
+        time=${tokens[3]}
+
+        # echo "${kc},${ts},${gc},${time}" >> $resultsfile
+        echo "${kc},${ts},${gc},${time}"
+    done
+
+echo "intersect tests"
+# echo "intersect tests" >> $resultsfile
+
+keycount=$((echo $keycount - 1) | bc)
+for i in "${tablesizes[@]}"
+    do
+        let kc=$((echo 2^$keycount) | bc)
+        # let gc=$((echo 2^$gpucount) | bc)
+        let gc=$gpucount
+        let ts=$((echo 2^$i) | bc)
+
+        # echo "tableSize: ${ts}"
+        # internal cuda malloc + keys + hashes + keyBinBuff
+        let gigs=$((echo "((($kc * $1) + ($kc * 8) + (2 * $kc * $1) + (2 * $ts * 8)) + ($kc * 8) + ($kc * 8) + ($kc * $1)) / 2^30") | bc)
+        let gpureq=$((echo "($gigs * 2 + 32) / 32") | bc)
+
+        if (( $gpureq > $gc )) ; then
+          echo "${kc},${gc},oom"
+          continue
+        fi
+        ans=$(./$execpath/multi-hash $kc $ts $bincount $gc $bincount nocheck $kc intersect | grep "time")
+
+        tokens=( $ans )
+        time=${tokens[3]}
+
+        # echo "${kc},${ts},${gc},${time}" >> $resultsfile
+        echo "${kc},${ts},${gc},${time}"
+    done
---
+++ hash-graph-dehornetify/experiments/dgx2_old/weak_scaling/weak_scaling.sh
@@ -0,0 +1,94 @@
+keycounts=($(seq 24 1 33))
+# gpucounts=($(seq 0 1 4))
+gpucounts=(1 2 4 8 16)
+# gpucounts=(1 2 4 8)
+
+execpath="../../build"
+resultsfile=$1
+
+bincount=16000
+
+# sed -i 's/^\/\/.*#define INDEX_TRACK/#define INDEX_TRACK/' ../../include/MultiHashGraph.cuh
+# make -C $execpath multi-hash
+
+# rm $resultsfile
+# echo "keycount,gpucount,time" >> $resultsfile
+echo "weak_scaling"
+echo "keycount,gpucount,time"
+echo "build tests"
+# echo "build tests" >> $resultsfile
+
+for i in "${keycounts[@]}"
+    do
+        let kcdev=$((echo 2^$i) | bc)
+        # echo "keycount / dev: ${kcdev}"
+        for j in "${gpucounts[@]}"
+            do
+                # let gc=$((echo 2^$j) | bc)
+                let gc=$j
+                # echo "gpucount: ${gc}"
+
+                let kc=$(($kcdev * $gc))
+                let ts=$kc
+                if [ $2 -eq 4 ] ; then
+                    ts=$(($kc < (2**($2 * 8)) ? $kc : 2**($2 * 8)))
+                fi
+
+                # internal cuda malloc + keys + hashes + keyBinBuff
+                let gigs=$((echo "((($kc * $1) + ($kc * $2) + (2 * $kc * $1) + (2 * $kc * 8)) + ($kc * $2) + ($kc * $2) + ($kc * $1)) / 2^30") | bc)
+                let gpureq=$((echo "($gigs + 32) / 32") | bc)
+
+                if (( $gpureq > $gc )) ; then
+                  echo "${kc},${gc},oom"
+                  continue
+                fi
+
+                # ans=$(./$execpath/multi-hash $kc $kc $bincount $gc $bincount nocheck $kc build | grep "time")
+                ans=$(./$execpath/multi-hash $kc $ts $bincount $gc $bincount nocheck $kc build | grep "time")
+                tokens=( $ans )
+                time=${tokens[3]}
+
+                # echo "${kc},${gc},${time}" >> $resultsfile
+                echo "${kc},${gc},${time}"
+            done
+    done
+
+echo "intersect tests"
+# echo "intersect tests" >> $resultsfile
+
+for i in "${keycounts[@]}"
+    do
+        let kcdev=$((echo 2^$i) | bc)
+        kcdev=$((kcdev / 2))
+        # echo "keycount / dev : ${kcdev}"
+        for j in "${gpucounts[@]}"
+            do
+                # let gc=$((echo 2^$j) | bc)
+                let gc=$j
+                # echo "gpucount: ${gc}"
+
+                let kc=$(($kcdev * $gc))
+                let ts=$kc
+                if [ $2 -eq 4 ] ; then
+                    ts=$(($kc < (2**($2 * 8)) ? $kc : 2**($2 * 8)))
+                fi
+
+                # # internal cuda malloc + keys + hashes + keyBinBuff
+                let gigs=$((echo "((($kc * $1) + ($kc * $2) + (2 * $kc * $1) + (2 * $kc * 8)) + ($kc * $2) + ($kc * $2) + ($kc * $1)) / 2^30") | bc)
+                let gpureq=$((echo "($gigs * 2 + 32) / 32") | bc)
+
+                if (( $gpureq > $gc )) ; then
+                  echo "${kc},${gc},oom"
+                  continue
+                fi
+
+                # ans=$(./$execpath/multi-hash $kc $kc $bincount $gc $bincount nocheck $kc intersect | grep "time")
+                ans=$(./$execpath/multi-hash $kc $ts $bincount $gc $bincount nocheck $kc intersect | grep "time")
+                tokens=( $ans )
+                time=${tokens[3]}
+
+                # echo "${kc},${gc},${time}" >> $resultsfile
+                echo "${kc},${gc},${time}"
+            done
+    done
+
